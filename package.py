88370fb8e418beb5ddd0ab5f9505ee61681ee3d8
hossnys <hossnys@greenitglobe.com>
2019-12-31 13:52:03 +0200

    adding package files

--- package.py
+++ package.py
@@ -1,53 +1,22 @@
 from Jumpscale import j
 
 class Package(j.baseclasses.threebot_package):
-    """
-    to start need to run 
-    kosmos -p "j.tools.threebot_packages.get('threefold_love_stag',giturl='https://github.com/threefold-love/www_threefold_love.git',branch='master')"
-    kosmos -p "j.servers.threebot.default.start(web=True, ssl=False)"
-    """
-    def _init(self, **kwargs):
-        self.branch = kwargs["package"].branch or "master"
-        self.threefold_love_stag = "https://github.com/threefold-love/www_threefold_love.git"
-
-    def prepare(self):
-        """
-        called when the 3bot starts
-        :return:
-        """
-        server = self.openresty
-        server.install(reset=True)
-        server.configure()
-        website = server.websites.get("threefold_love_stag")
-        website.ssl = False
-        website.port = 80
-        locations = website.locations.get("threefold_love_stag")
-        static_location = locations.locations_static.new()
-        static_location.name = "static"
-        static_location.path_url = "/"
-        path = j.clients.git.getContentPathFromURLorPath(self.threefold_love_stag, branch=self.branch, pull=True)
-        static_location.path_location = path
-        static_location.use_jumpscale_weblibs = True
-        website.path = path
-        locations.configure()
-        website.configure()
 
     def start(self):
-        """
-        called when the 3bot starts
-        :return:
-        """
-        self.prepare()
-    def stop(self):
-        """
-        called when the 3bot stops
-        :return:
-        """
-        pass
+        server = self.openresty
+        server.configure()
+        for port in [80, 443]:
+            website = server.get_from_port(port)
+            website.domain = "www.threefold.love"
+            locations = website.locations.get(f"threefold_love_locations_{port}")
 
-    def uninstall(self):
-        """
-        called when the package is no longer needed and will be removed from the threebot
-        :return:
-        """
-        pass
+            website_location = locations.locations_static.new()
+            website_location.name = "threefold_love_website"
+            website_location.path_url = "/"
+            fullpath = j.sal.fs.joinPaths(self.package_root, "html/")
+            website_location.path_location = fullpath
+
+            locations.configure()
+            website.configure()
+            website.save()
+
